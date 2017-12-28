eea30b4fe1c8d5029296a7e8129350582e4f2219
Tang Yusi <2080466270@qq.com>
2017-12-29 01:55:09 +0800

    test

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '118.89.46.254',
     // 数据库名
     'database'        => 'longjing',
     // 用户名
